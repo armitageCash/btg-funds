957dee6be415b89f3203f3d978119f054a792f40
Andrew Castro <aacastro661989@gmail.com>
2024-10-16 08:59:36 -0500

    remove logger

--- src/infrastructure/http/requestSchemas/subscrition.ts
+++ src/infrastructure/http/requestSchemas/subscrition.ts
@@ -1,10 +1,19 @@
 import Joi from "joi";
 
 export const SubscriptionSchema = Joi.object({
-  user: Joi.string().guid({ version: ["uuidv4"] }), // Validación del objeto User
-  fund: Joi.string().guid({ version: ["uuidv4"] }), // Validación del objeto Fund
-  amount: Joi.number().positive().required(), // Debe ser un número positivo
+  user: Joi.string()
+    .guid({ version: ["uuidv4"] })
+    .required()
+    .message("El usuario es requerido"), // Validación del objeto User
+  fund: Joi.string()
+    .guid({ version: ["uuidv4"] })
+    .required()
+    .message("El fondo es requerido"), // Validación del objeto Fund
+  amount: Joi.number().positive().required().message("El monto es requerido"), // Debe ser un número positivo
   createdAt: Joi.date().iso().optional(), // Opcional y debe ser una fecha en formato ISO
   updatedAt: Joi.date().iso().optional(), // Opcional y debe ser una fecha en formato ISO
-  status: Joi.string().valid("Opened", "Closed").required(), // Solo permite los valores "Opened" o "Closed"
+  status: Joi.string()
+    .valid("Opened", "Closed")
+    .required()
+    .message("el campo status es requerido"), // Solo permite los valores "Opened" o "Closed"
 });
